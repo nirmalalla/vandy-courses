c1ad3a77ff5263fe81e90ea7429aafa7e27e60b6
nirmalalla <allanirmal@gmail.com>
2024-12-14 17:54:31 -0600

    adding search autocomplete

--- client/src/app/Components/Searchbar.tsx
+++ client/src/app/Components/Searchbar.tsx
@@ -1,9 +1,21 @@
 'use client'
 
 import { useEffect, useState } from "react";
+import { AutoComplete } from "antd";
+
+interface CourseName{
+    course?: string
+}
+
+interface Option{
+    value?: string
+}
 
 export default function SearchBar(){
-    const [options, setOptions] = useState([]);
+    const [allOptions, setAllOptions] = useState<Option[]>([]);
+    const [filteredOptions, setFilteredOptions] = useState<Option[]>([]);
+    const [chosen, setChosen] = useState("");
+
     const getCourses = async () => {
         const res = await fetch("http://localhost:5000/api/grades/course", {
             method: "GET",
@@ -13,18 +25,46 @@ export default function SearchBar(){
         });
 
         const data = await res.json();
-        const courses = data.map((item: any) => item.course);
-        console.log(courses);
-        setOptions(courses);
+        const courses = data.map((item: CourseName) => ({
+            value: item.course,
+        }));
+        
+        setAllOptions(courses);
+        setFilteredOptions(courses);
+    }
+
+    const getPanelValue = (searchText: string) => {
+        if (!searchText) return allOptions;
+
+        return allOptions.filter((option: Option) => 
+            option?.value?.toLowerCase().includes(searchText.toLowerCase())
+        );
+    }
+
+    const onChange = (data: string) => {
+        setChosen(data);
+    }
+
+    const onSearch = (text: string) => {
+        const filtered = getPanelValue(text);
+        setFilteredOptions(filtered);
     }
 
     useEffect(() => {
         getCourses();
-    })
+    }, [])
 
     return (
         <>
-            <p>test</p>
+            <AutoComplete
+                value={chosen} 
+                options={filteredOptions}
+                style={{width: 200}}
+                onChange={onChange}
+                onSearch={onSearch}
+                placeholder="search course"
+                size="large"
+            />
         </>
     )
 }
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,21 @@
 import Searchbar from "./Components/Searchbar";
+import { Layout } from "antd";
+import { Header } from "antd/es/layout/layout";
+import { Content } from "antd/es/layout/layout";
+import { Footer } from "antd/es/layout/layout";
+
 
 export default function Home() {
   return (
     <>
-      <Searchbar />
+      <Layout>
+        <Header style={{ display: "flex", alignItems: "center"}}>
+        </Header>
+        <Content style={{ padding: '0 48px'}}>
+          <Searchbar />
+        </Content>
+        <Footer style={{ textAlign: "center"}}>made by nirmal</Footer>
+      </Layout>
     </>
   )
 }
