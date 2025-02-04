a43cb3e607b5547d73b2ed824535802d9be9e6cc
nirmalalla <allanirmal@gmail.com>
2025-02-04 14:17:36 -0600

    reconfig for db

--- server/config/database.ts
+++ server/config/database.ts
@@ -1,6 +1,9 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize({
+    database: process.env.DB_NAME,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT),
     logging: console.log,
