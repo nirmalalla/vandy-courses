12175dfc909128db59281814859a141ac78e5c9e
nirmalalla <allanirmal@gmail.com>
2025-02-04 17:57:59 -0600

    debugging

--- server/middleware.ts
+++ server/middleware.ts
@@ -53,7 +53,9 @@ function parseCookies(cookieString: string): {
 
 export const authenticate = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
     const cookie = req.headers.cookie;
-
+    console.log(cookie);
+    console.log(req.headers);
+    
     if (!cookie || cookie.length === 0){
         res.status(401).json({error: "missing cookie"});
         return;
@@ -61,7 +63,6 @@ export const authenticate = async (req: AuthenticatedRequest, res: Response, nex
 
 
     const { authToken, userInfo } = parseCookies(cookie); 
-    console.log(authToken);
     if (!authToken){
         res.status(401).json({error: "missing token"});
     }
